1c8ba406124a9e4adab7578255c82f04fa64c5f2
kohlten <32156797+kohlten@users.noreply.github.com>
2017-11-29 00:19:05 -0700

    Add files via upload

--- src/extract.py
+++ src/extract.py
@@ -5,36 +5,7 @@ start_time = time()
 import pylzma, sys, os
 from Crypto.Cipher import AES
 import cPickle
-
-
-
-def decrypt(key, text, iv):
-    # # Check the key and iv are correct
-    if len(key) not in [16, 24, 32]:
-        raise ValueError, "Key must be 16, 24, or 32 characters long!"
-
-    if len(iv) not in [16, 24, 32]:
-        raise ValueError, "Iv must be 16, 24, or 32 characters long!"
-
-    # # If the length of the text is not a multiple of 16
-    # # Add zeros until it is
-    characters = 0
-    if len(text) % 16 != 0:
-        text += "0"
-        characters += 1
-
-    # # Create a new encryption object and decrypt the text
-    obj = AES.new(key, AES.MODE_CFB, iv)
-    out = obj.decrypt(text)
-
-    # # If the text is changed, get rid of the extra text
-    if characters > 0:
-        out = out[0:len(out) - characters]
-
-    return out, key, iv
-
-# # Get the args from the command line
-args = sys.argv
+from encrypt import decrypt
 
 help = """Extract tool for Coda
 Meant to be used alongside a download to extract the files.
@@ -49,8 +20,12 @@ __________________________________________________________________
 # # BOTH the key and iv are required to decrypt the files.
 __________________________________________________________________"""
 
+
 "TO DO"
 # # -extract_location = The place to put the files when done"
+
+# # Get the args from the command line
+args = sys.argv
 
 if __name__ == "__main__":
     # # Display the help
@@ -58,21 +33,20 @@ if __name__ == "__main__":
         print(help)
         sys.exit(0)
 
-    # # If -name in args, use that name. Otherwise use "out".
-    name = ""
-    if "-name" in args:
-        name = args[args.index("-name") + 1]
-    else:
-        for file_name in os.listdir(os.getcwd()):
-            print(file_name)
+    # # If -name in args, use that name. Otherwise find the file in the current folder.
+    name = None
+    if "-name" not in args:
+    	for file_name in os.listdir(os.getcwd()):
             if len(file_name.split(".")) > 1 and file_name.split(".")[1] == "coda":
                 print("Found!")
                 name = file_name
+    if "-name" in args:
+	name = args[args.index("-name") + 1]
 
-    if name == "":
+    if name is None:
         print("ERROR 2: Unable to find a coda file.")
         sys.exit(2)
-    # # Open the compressed file and the info file
+    # # Open the compressed file
     try:
         pickle_in = open(name, "rb")
     except IOError:
@@ -81,7 +55,6 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     # # Decompress the info and decompress the file names
-    # # If the info is corrupt don't continue
     pickle_in = cPickle.load(pickle_in)
     try:
         names = pylzma.decompress(pickle_in[1][0]).split(",")
@@ -117,8 +90,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # # Write the text to the associated files
-    for i in range(len(names)):
-        file = open(names[i], "wb")
+    for name in names:
+        file = open(name, "wb")
         file.write(files[i])
         file.close()
 
